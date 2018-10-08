be5460dc634b6f68268484c2e0007043a9841432
Marko Melnick <M@cu-ramaley-128.138.220.76.colorado.edu>
2018-10-08 12:50:56 -0600

    cleaned up some files

--- fig6_J2_Enrichment_DOG/fig6.5_J2_Venn_and_pieplot/sense_antisense_fc.py
+++ fig6_J2_Enrichment_DOG/fig6.5_J2_Venn_and_pieplot/sense_antisense_fc.py
@@ -16,20 +16,11 @@ from matplotlib_venn import venn2, venn2_circles
 
 
 
-# f1 = "HS_combined_LONGEST_RUN_ON.csv"
-# f2 = "HS_combined_antisense_LONGEST_RUN_ON.csv"
-#
-# f3 = "OK_combined_LONGEST_RUN_ON.csv"
-# f4 = "OK_combined_antisense_LONGEST_RUN_ON.csv"
+f1 = "HS_combined_DOG_noOperon_unique_cleaned.csv"
+f2 = "HS_combined_ADOG_noOperon_unique_cleaned.csv"
 
-
-f1 = "../unique_DOG/HS_combined_DOG_noOperon_unique_cleaned.csv"
-f2 = "../unique_DOG/HS_combined_ADOG_noOperon_unique_cleaned.csv"
-f3 = "../unique_DOG/OK_combined_DOG_noOperon_unique_cleaned.csv"
-f4 = "../unique_DOG/OK_combined_ADOG_noOperon_unique_cleaned.csv"
-
-
-
+f3 = "OK_combined_DOG_noOperon_unique_cleaned.csv"
+f4 = "OK_combined_ADOG_noOperon_unique_cleaned.csv"
 
 
 
@@ -40,6 +31,7 @@ def get_above_below_2lfc_p05(f1):
     """This function will take in a DSeq2 normalized matrix and filter for everything LT 0.05
     Give back two df of above 2lfc and below 2lfc"""
     df = pd.read_csv(f1, index_col=None, sep=',')
+    print("reading : ", f1)
     #df.rename(columns={"Unnamed: 0" : "gene_id"}, inplace=True)
     # df = df[df["baseMean"] > 20]
     # df = df[df["gene_biotype_name"] != "rRNA"]
@@ -55,14 +47,14 @@ df_HS_anti_sense_above_2_lfc, df_HS_anti_sense_below_2_lfc = get_above_below_2lf
 df_OK_sense_above_2_lfc, df_OK_sense_below_2_lfc = get_above_below_2lfc_p05(f3)
 df_OK_anti_sense_above_2_lfc, df_OK_anti_sense_below_2_lfc = get_above_below_2lfc_p05(f4)
 
-df_HS_sense_above_2_lfc.to_csv("df_HS_sense_above_padj.05.csv",sep="\t",index=None)
-df_HS_sense_below_2_lfc.to_csv("df_HS_sense_below_padj.05.csv",sep="\t",index=None)
-df_OK_sense_above_2_lfc.to_csv("df_OK_sense_above_padj.05.csv",sep="\t",index=None)
-df_OK_sense_below_2_lfc.to_csv("df_OK_sense_below_padj.05.csv",sep="\t",index=None)
-df_HS_anti_sense_above_2_lfc.to_csv("df_HS_anti_sense_above_padj.05.csv",sep="\t",index=None)
-df_HS_anti_sense_below_2_lfc.to_csv("df_HS_anti_sense_below_padj.05.csv",sep="\t",index=None)
-df_OK_anti_sense_above_2_lfc.to_csv("df_OK_anti_sense_above_padj.05.csv",sep="\t",index=None)
-df_OK_anti_sense_below_2_lfc.to_csv("df_OK_anti_sense_below_padj.05.csv",sep="\t",index=None)
+df_HS_sense_above_2_lfc.to_csv("df_HS_sense_above.csv",sep="\t",index=None)
+df_HS_sense_below_2_lfc.to_csv("df_HS_sense_below.csv",sep="\t",index=None)
+df_OK_sense_above_2_lfc.to_csv("df_OK_sense_above.csv",sep="\t",index=None)
+df_OK_sense_below_2_lfc.to_csv("df_OK_sense_below.csv",sep="\t",index=None)
+df_HS_anti_sense_above_2_lfc.to_csv("df_HS_anti_sense_above.csv",sep="\t",index=None)
+df_HS_anti_sense_below_2_lfc.to_csv("df_HS_anti_sense_below.csv",sep="\t",index=None)
+df_OK_anti_sense_above_2_lfc.to_csv("df_OK_anti_sense_above.csv",sep="\t",index=None)
+df_OK_anti_sense_below_2_lfc.to_csv("df_OK_anti_sense_below.csv",sep="\t",index=None)
 
 
 df_HS_sense_above_2_lfc_GO = df_HS_sense_above_2_lfc["GeneID"]
@@ -135,8 +127,8 @@ v.get_patch_by_id('11').set_alpha(0.7)
 c = venn2_circles(subsets=s, linestyle='solid')
 c[0].set_ls('dashed')  # Line style
 c[0].set_lw(2.0)       # Line width
-# plt.title("Heatshock and tdp-1(ok803) \n Sense J2 Enrichment compared to WT \n  log2FoldChange > 0 \n (Padj) < 0.05", fontsize=20)
-plt.savefig('Heatshock and tdp-1(ok803) Sense J2 Enrichment compared to WT.png',bbox_inches='tight')
+plt.title("RUNON Heatshock and tdp-1(ok803) \n Sense J2 Enrichment compared to WT \n  log2FoldChange > 1 \n (Padj) < 0.05, \n basemean >20", fontsize=20)
+plt.savefig('RUNON Heatshock and tdp-1(ok803) Sense J2 Enrichment compared to WT.png',bbox_inches='tight')
 #savefig('foo.png', bbox_inches='tight')
 #plt.show()
 
@@ -218,8 +210,8 @@ v.get_patch_by_id('11').set_alpha(0.7)
 c = venn2_circles(subsets=s, linestyle='solid')
 c[0].set_ls('dashed')  # Line style
 c[0].set_lw(2.0)       # Line width
-# plt.title("Antisense Heatshock and tdp-1(ok803) \n Sense J2 Enrichment compared to WT \n  log2FoldChange > 0 \n (Padj) < 0.05", fontsize=20)
-plt.savefig('Antisense Heatshock and tdp-1(ok803) Sense J2 Enrichment compared to WT.png',bbox_inches='tight')
+plt.title("AntisenseRUNON Heatshock and tdp-1(ok803) \n Sense J2 Enrichment compared to WT \n  log2FoldChange > 0 \n (Padj) < 0.05, \n basemean >20", fontsize=20)
+plt.savefig('Antisense RUNON Heatshock and tdp-1(ok803) Sense J2 Enrichment compared to WT.png',bbox_inches='tight')
 #savefig('foo.png', bbox_inches='tight')
 #plt.show()
 
